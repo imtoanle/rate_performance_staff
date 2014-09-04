d8917d4f11cb81cfbaf767c1706b3e6e7046f3fe
Toàn Lê <ktoanlba@gmail.com>
2014-09-04 12:44:31 +0700

    Add 2 file migration

---
+++ app/database/migrations/2014_09_04_051256_create_table_test.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTest extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tests', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('value')->default(5);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tests');
+	}
+
+}
