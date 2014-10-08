f68cff9c294c334d53d061fbf5b4ee41ca8e4abc
Toàn Lê <ktoanlba@gmail.com>
2014-10-08 12:15:12 +0700

    Fix user seed db

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,6 +6,7 @@ class UserTableSeeder extends Seeder {
   {
     $faker = Faker\Factory::create();
 
+    /*
     for ($i=1;$i<=1;$i++)
     {
       $username = $i==1 ? 'admin' : $faker->userName;
@@ -35,6 +36,20 @@ class UserTableSeeder extends Seeder {
       $user->addGroup($randomGroup);
 
     }
+    */
+
+    $user = Sentry::createUser(array(
+        'email' => $faker->email,
+        'username' => 'admin',
+        'password' => '123123',
+        'activated' => true,
+        'full_name' => $faker->name($gender = NULL),
+        'phone_num' => $faker->phoneNumber,
+        'birth_date' => $faker->dateTimeThisCentury($max = 'now'),
+        ));
+
+      $adminGroup = Sentry::findGroupById(1);
+      $user->addGroup($adminGroup);
   }
  
 }
